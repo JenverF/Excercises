1ed3206e1bbb63eca74c9ac485572586ef016f60
JenverF <jenver.dfernandez@gmail.com>
2025-09-23 12:05:02 -0700

    Data types

---
+++ src/main/java/com/pluralsight/Datatypes.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        int vehicleID = 1234567;
+        System.out.println(vehicleID);
+        String vehiclemodel = "Volkswagon Bettle";
+        System.out.println(vehiclemodel);
+        String vehiclecolor = "Yellow";
+        System.out.println(vehiclecolor);
+        boolean hasTowingPackage = true;
+        System.out.println(hasTowingPackage);
+        int odometer = 654321;
+        System.out.println(odometer);
+        double price = 67.41;
+        System.out.println(price);
+        char qualityrating = 'B';
+        System.out.println(qualityrating);
+        int phoneNumber = 123-45-6789;
+        System.out.println(phoneNumber);
+        String ssn = "123-45-6789";
+        System.out.println(ssn);
+        String zipcode = "98178";
+        System.out.println(zipcode);
+    }
+}
