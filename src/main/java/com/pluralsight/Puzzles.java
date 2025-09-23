a7c2082e9b6f5ee6b6f4ae99dabce2afbec7f4e3
JenverF <jenver.dfernandez@gmail.com>
2025-09-23 14:50:45 -0700

    Puzzles for outputs

---
+++ src/main/java/com/pluralsight/Puzzles.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+public class Puzzles {
+    public static void main(String[] args) {
+        int a = 4, b = 6;
+        System.out.println(a++ + ++b);
+        int x = 5, y = 3, z = 2;
+        System.out.println(x++ + ++y * z++);
+        int eggs = 150, perBox = 12;
+        System.out.println(eggs / perBox + " " + eggs % perBox);
+        int n = 10;
+        n += n++ + ++n;
+        System.out.println(n);
+        int p = 7;
+        System.out.println(p++ + p++ + ++p);
+        int u = 2, v = 5;
+        System.out.println(++u * v-- - u++);
+        int gold = 10, players = 3;
+        double avg = gold / players;
+        System.out.println(avg);
+        int m = 5, d = 3;
+        System.out.println(m / d * d + m % d);
+        int lvl = 1;
+        lvl = lvl++ + ++lvl + lvl;
+        System.out.println(lvl);
+        int A = 5, B = 3, C = 2;
+        System.out.println(A++ * --B + C++ / B);
+    }
+}
