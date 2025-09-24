de99d2dd3134f37d0fae40aae77ac6865cc9fca4
JenverF <jenver.dfernandez@gmail.com>
2025-09-24 15:53:30 -0700

    More exercises

---
+++ src/main/java/com/pluralsight/week1/BasicCalulator.java
@@ -0,0 +1,19 @@
+package com.pluralsight.week1;
+import java.util.Scanner;
+
+public class BasicCalulator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your favorite animal?");
+        String animal = scanner.next();
+        System.out.println("First tool that comes to mind");
+        String tool = scanner.next();
+        System.out.println("Day you were born");
+        int day = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.printf("Your favorite animal is %s, first tool that comes into mind is a %s and %d is the day you were born.", animal, tool, day);
+
+
+    }
+}
