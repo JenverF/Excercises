fe68d24dbc75469841ac5a1d24a97aef47ec3a8a
JenverF <jenver.dfernandez@gmail.com>
2025-09-24 14:02:52 -0700

    Math exercises

--- src/main/java/com/pluralsight/week1/MathExcercises.java
+++ src/main/java/com/pluralsight/week1/MathExcercises.java
@@ -1,4 +1,4 @@
-package com.pluralsight;
+package com.pluralsight.week1;
 
 public class MathExcercises {
     public static void main(String[] args) {
--- src/main/java/com/pluralsight/week1/Puzzles.java
+++ src/main/java/com/pluralsight/week1/Puzzles.java
@@ -1,4 +1,4 @@
-package com.pluralsight;
+package com.pluralsight.week1;
 
 public class Puzzles {
     public static void main(String[] args) {
