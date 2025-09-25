d011db1f87b5c9d275a75007d474678cb27f29f5
JenverF <jenver.dfernandez@gmail.com>
2025-09-25 10:58:34 -0700

    More exercises

--- src/main/java/com/pluralsight/week1/Payroll.java
+++ src/main/java/com/pluralsight/week1/Payroll.java
@@ -8,10 +8,15 @@ public class Payroll {
         System.out.println("What is your name: ");
         String name = scanner.nextLine();
         System.out.print("How many hours do you work: ");
-        float hours = scanner.nextFloat();
+        double hours = scanner.nextDouble();
         System.out.print("What is your pay rate: ");
-        float payRate = scanner.nextFloat();
-        float grossPay = hours * payRate;
+        double payRate = scanner.nextDouble();
+        double grossPay = 0;
+        if(hours >= 40) {
+            grossPay = (hours - 40) * payRate * 1.5 + 40 * payRate;
+        } else {
+            grossPay = payRate * hours;
+        }
         System.out.printf("Hi %s, your gross pay would be $%.2f.", name, grossPay);
 
     }
