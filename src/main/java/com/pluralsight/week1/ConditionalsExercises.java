be937224d1056fe271cd5375ab46cb5d0d9235bb
JenverF <jenver.dfernandez@gmail.com>
2025-09-25 15:54:28 -0700

    Sandwich Shop

--- src/main/java/com/pluralsight/week1/ConditionalsExercises.java
+++ src/main/java/com/pluralsight/week1/ConditionalsExercises.java
@@ -44,7 +44,7 @@ public class ConditionalsExercises {
         } else {
             System.out.println("Safe speed");
         }
-        double leapYear = 2028;
+        double leapYear = 2000;
         if(leapYear % 4 ==0 && leapYear % 100 != 0) {
             System.out.println("Leap year");
         } else {
