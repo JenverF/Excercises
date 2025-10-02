ea6bad44d065ae6ccc4a27b0711d3fea3baacbeb
JenverF <jenver.dfernandez@gmail.com>
2025-10-02 10:48:48 -0700

    More exercises

---
+++ src/main/java/com/pluralsight/week2/CellPhone.java
@@ -0,0 +1,61 @@
+package com.pluralsight.week2;
+
+public class CellPhone {
+    private long serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+
+    public CellPhone() {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+        }
+
+    public long getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(long serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void dial(String phoneParameter) {
+        System.out.println(owner + " phone is calling " + phoneParameter);
+    }
+}
