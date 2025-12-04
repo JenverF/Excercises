88849183c792bb62a1270dad9e4ebfa0ac7e98b7
JenverF <jenver.dfernandez@gmail.com>
2025-12-04 08:47:24 -0800

    More exercises

--- src/main/java/com/pluralsight/workbook8/NorthwindTraders.java
+++ src/main/java/com/pluralsight/workbook8/NorthwindTraders.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class NorthwindTraders {
     static Scanner scanner = new Scanner(System.in);
     static void main(String[] args) {
-        displayHomeScreen();;
+        displayHomeScreen();
         int choice = Integer.parseInt(getUserInput("Select an option:"));
         switch(choice) {
             case 1:
@@ -15,17 +15,23 @@ public class NorthwindTraders {
             case 2:
                 displayAllCustomers(args);
                 break;
+            case 3:
+                displayAllCategories(args);
+                displayProductsByCategoryId(args);
+                break;
             default:
                 System.out.println("Not available now.");
+                break;
         }
     }
 
     public static void displayHomeScreen() {
         String options = """
                 What do you want to do?
-                1) Display all products
-                2) Display all customers
-                0) Exit
+                    1) Display all products
+                    2) Display all customers
+                    3) Display all categories
+                    0) Exit
                 """;
         System.out.println(options);
     }
@@ -80,6 +86,38 @@ public class NorthwindTraders {
                         """, contactName, companyName, city, country, phone);
             }
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void displayAllCategories(String[] args) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", args[0], args[1]);
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM categories ORDER BY CategoryID");
+             ResultSet resultSet = statement.executeQuery();
+        ) {
+
+            while (resultSet.next()) {
+                System.out.printf("Category ID: %s | Category name: %s\n", resultSet.getString("CategoryID"), resultSet.getString("CategoryName"));
+            }
+            ;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+        public static void displayProductsByCategoryId(String[] args) {
+        int catId = Integer.parseInt(getUserInput("Which category do you want to see? Please enter the ID:"));
+        try( Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", args[0], args[1]);
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM Products WHERE CategoryID = ?");
+        ) {
+
+            statement.setInt(1, catId);
+            try(ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    System.out.printf("Product ID: %s | Product name: %s | Unit Price: %.2f | Units in stock: %d\n", resultSet.getString("ProductId"), resultSet.getString("ProductName"), resultSet.getFloat("UnitPrice"), resultSet.getInt("UnitsInStock"));
+                }
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
